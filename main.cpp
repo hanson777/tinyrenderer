19826900cb48d9fb4747cb8b985e6de707b84d50
hanson777 <154561030+hanson777@users.noreply.github.com>
2026-03-06 11:15:12 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include "tgaimage.h"
+
+constexpr TGAColor white = { 255, 255, 255, 255 }; // attention, BGRA order
+constexpr TGAColor green = { 0, 255, 0, 255 };
+constexpr TGAColor red = { 0, 0, 255, 255 };
+constexpr TGAColor blue = { 255, 128, 64, 255 };
+constexpr TGAColor yellow = { 0, 200, 255, 255 };
+
+void line(int ax, int ay, int bx, int by, TGAImage& framebuffer, const TGAColor& color) {
+    bool steep = std::abs(ax - bx) < std::abs(ay - by);
+    if (steep) {
+        std::swap(ax, ay);
+        std::swap(bx, by);
+    }
+    if (ax > bx) {
+        std::swap(ax, bx);
+        std::swap(ay, by);
+    }
+    for (int x = ax; x < bx; x++) {
+        float t = (x - ax) / static_cast<float>(bx - ax);
+        int y = ay + t * (by - ay);
+        if (steep) // mirror again if mirrored previously
+            framebuffer.set(y, x, color);
+        else framebuffer.set(x, y, color);
+    }
+    // float distance = std::sqrt(std::abs(ax - bx) * std::abs(ax - bx) + std::abs(ay - by) * std::abs(ay - by));
+    // float delta = 1 / distance;
+    // for (float t = 0.0f; t <= 1.0f; t += delta) {
+    //     int x = ax + t * (bx - ax);
+    //     int y = ay + t * (by - ay);
+    //     framebuffer.set(x, y, color);
+    // }
+}
+
+int main(int argc, char** argv) {
+    constexpr int width = 64;
+    constexpr int height = 64;
+    TGAImage framebuffer(width, height, TGAImage::RGB);
+
+    int ax = 7, ay = 3;
+    int bx = 12, by = 37;
+    int cx = 62, cy = 53;
+
+    line(ax, ay, bx, by, framebuffer, blue);
+    line(cx, cy, bx, by, framebuffer, green);
+    line(cx, cy, ax, ay, framebuffer, yellow);
+    line(ax, ay, cx, cy, framebuffer, red);
+
+    framebuffer.set(ax, ay, white);
+    framebuffer.set(bx, by, white);
+    framebuffer.set(cx, cy, white);
+
+    framebuffer.write_tga_file("framebuffer.tga");
+    return 0;
+}
